7d02c037f4133988868a7650acb68c0d8c534760
miqilin21 <994718917@qq.com>
2020-03-17 23:26:26 +0800

    feat: 后台基本界面

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,8 +1,25 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from './router'
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import './style.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
+
+import http from "./http";
+Vue.prototype.$http = http;
+
+Vue.mixin({
+  computed: {
+    uploadUrl() {
+      return this.$http.defaults.baseURL + '/upload'
+    }
+  },
+})
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express()
+
+app.use(require('cors')())
+app.use(express.json())
+app.use('/uploads', express.static(__dirname + '/uploads'))
+
+
+require('./plugins/db')(app)
+require('./routes/admin')(app)
+
+app.listen(3000, () => {
+  console.log('http://localhost:3000');
+})
