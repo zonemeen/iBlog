4d88e0567e99e11158cc4f4630f59ce227713d4f
miqilin21 <994718917@qq.com>
2020-04-05 19:26:08 +0800

    feat: aliyun oss

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,10 +1,10 @@
-module.exports = app => {
+module.exports = (app) => {
   const express = require("express");
-  const assert = require('http-assert')
-  const jwt = require('jsonwebtoken')
-  const AdminUser = require('../../models/AdminUser')
+  const assert = require("http-assert");
+  const jwt = require("jsonwebtoken");
+  const AdminUser = require("../../models/AdminUser");
   const router = express.Router({
-    mergeParams: true
+    mergeParams: true,
   });
   router.post("/", async (req, res) => {
     const model = await req.Model.create(req.body);
@@ -17,7 +17,7 @@ module.exports = app => {
   router.delete("/:id", async (req, res) => {
     await req.Model.findByIdAndDelete(req.params.id, req.body);
     res.send({
-      success: true
+      success: true,
     });
   });
   router.get("/", async (req, res) => {
@@ -25,9 +25,7 @@ module.exports = app => {
     if (req.Model.modelName === "Category") {
       queryOptions.populate = "parent";
     }
-    const items = await req.Model.find()
-      .setOptions(queryOptions)
-      .limit(100);
+    const items = await req.Model.find().setOptions(queryOptions).limit(100);
     res.send(items);
   });
   router.get("/:id", async (req, res) => {
@@ -51,8 +49,16 @@ module.exports = app => {
 
   //图片上传
   const multer = require("multer");
+  const MAO = require("multer-aliyun-oss");
   const upload = multer({
-    dest: __dirname + "/../../uploads"
+    storage: MAO({
+      config: {
+        region: "oss-cn-shenzhen",
+        accessKeyId: "LTAI4Feb6mXYySq4KFCFU8kh",
+        accessKeySecret: "p5GNOJjBbPb8Oj560JO4GPIqyc27CF",
+        bucket: "miqilin-blog",
+      },
+    }),
   });
   app.post(
     "/admin/api/upload",
@@ -60,36 +66,36 @@ module.exports = app => {
     upload.single("file"),
     async (req, res) => {
       const file = req.file;
-      file.url = `http://miqilin21.cn/uploads/${file.filename}`;
+      // file.url = `http://miqilin21.cn/uploads/${file.filename}`;
       res.send(file);
     }
   );
 
   //登录
-  app.post('/admin/api/login', async (req, res) => {
-    const {
-      username,
-      password
-    } = req.body
+  app.post("/admin/api/login", async (req, res) => {
+    const { username, password } = req.body;
     const user = await AdminUser.findOne({
-      username
-    }).select('+password')
-    assert(user, 422, '用户不存在')
-    const isValid = require('bcryptjs').compareSync(password, user.password)
-    assert(isValid, 422, '密码错误')
-    const token = jwt.sign({
-      id: user._id
-    }, app.get('secret'))
+      username,
+    }).select("+password");
+    assert(user, 422, "用户不存在");
+    const isValid = require("bcryptjs").compareSync(password, user.password);
+    assert(isValid, 422, "密码错误");
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      app.get("secret")
+    );
     res.send({
       token,
-      username
-    })
-  })
+      username,
+    });
+  });
 
   //错误处理函数
   app.use(async (err, req, res, next) => {
     res.status(err.statusCode || 500).send({
-      message: err.message
-    })
-  })
+      message: err.message,
+    });
+  });
 };
