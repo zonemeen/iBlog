7073d0b70cda505c4d5d86b3b66352daeab1d286
miqilin21 <994718917@qq.com>
2020-05-04 23:42:09 +0800

    fix: 跳转路由回至顶部

--- web/src/App.vue
+++ web/src/App.vue
@@ -5,5 +5,14 @@
 </template>
 
 <script>
-export default {};
+export default {
+  name: "app",
+  watch: {
+    $route: function(to, from) {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    },
+  },
+  methods: {},
+};
 </script>
