7638a1f74c6c8a33b3c8e928781f694e4971e0bb
miqilin21 <994718917@qq.com>
2020-05-24 09:06:47 +0800

    refactor: 背景动画改用sakura

--- web/src/components/snow.vue
+++ web/src/components/snow.vue
@@ -6,114 +6,199 @@
 export default {
   methods: {
     canvas() {
-      var flakes = [],
-        canvas = document.getElementById("Snow"),
-        ctx = canvas.getContext("2d"),
-        flakeCount = 150,
-        mX = -100,
-        mY = -100;
+      window.requestAnimFrame = (function() {
+        return (
+          window.requestAnimationFrame ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame ||
+          window.oRequestAnimationFrame ||
+          window.msRequestAnimationFrame ||
+          function(callback, element) {
+            window.setTimeout(callback, 1000 / 60);
+          }
+        );
+      })();
 
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
+      function random(n) {
+        return Math.floor(Math.random() * n) + 1;
+      }
 
-      function snow() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+      function Canvas(elm) {
+        this.elm = elm;
+        this.canvasCtx = this.elm.getContext("2d");
+        this.width = this.elm.width;
+        this.height = this.elm.height;
+        this.children = new Array();
 
-        for (var i = 0; i < flakeCount; i++) {
-          var flake = flakes[i],
-            x = mX,
-            y = mY,
-            minDist = 200,
-            x2 = flake.x,
-            y2 = flake.y;
+        this.init();
+      }
+      Canvas.prototype = {
+        resize: function(booleam) {
+          this.width = this.elm.width = booleam
+            ? this.elm.parentNode.clientWidth * 2
+            : window.innerWidth * 2;
+          this.height = this.elm.height = booleam
+            ? this.elm.parentNode.clientHeight * 2
+            : window.innerHeight * 2;
+        },
+        clear: function() {
+          this.canvasCtx.clearRect(0, 0, this.width, this.height);
+        },
+        addChild: function(child) {
+          this.children.push(child);
+        },
+        removeChild: function(num) {
+          this.children.splice(num, 1);
+        },
+        rendering: function() {
+          this.clear();
 
-          var dist = Math.sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y)),
-            dx = x2 - x,
-            dy = y2 - y;
-
-          if (dist < minDist) {
-            var force = minDist / (dist * dist),
-              xcomp = (x - x2) / dist,
-              ycomp = (y - y2) / dist,
-              deltaV = force / 2;
-
-            flake.velX -= deltaV * xcomp;
-            flake.velY -= deltaV * ycomp;
-          } else {
-            flake.velX *= 0.98;
-            if (flake.velY <= flake.speed) {
-              flake.velY = flake.speed;
+          var limit = this.children.length;
+          for (var i = limit - 1; i >= 0; i--) {
+            var child = this.children[i];
+            if (child.draw(this.canvasCtx)) {
+              this.removeChild(i);
             }
-            flake.velX += Math.cos((flake.step += 0.05)) * flake.stepSize;
+          }
+        },
+        createSakura: function(num, x1, y1, x2, y2) {
+          for (var i = 0; i < num; i++) {
+            var x_pos = Math.floor(Math.random() * (x2 - x1)) + x1;
+            var y_pos = Math.floor(Math.random() * (y2 - y1)) + y1;
+            this.addChild(
+              new Sakura(
+                this,
+                x_pos,
+                y_pos,
+                Math.random() + 0.7,
+                { x: random(360), y: random(360), z: random(360) },
+                { x: random(10), y: random(10), z: random(10) },
+                random(5)
+              )
+            );
+          }
+        },
+        animate: function() {
+          var _this = this;
+
+          if (Math.random() > 0.15 && this.children.length < 30) {
+            this.createSakura(1, 1, 1, this.width, 0);
           }
 
-          ctx.fillStyle = "rgba(255,255,255," + flake.opacity + ")";
-          flake.y += flake.velY;
-          flake.x += flake.velX;
+          this.rendering();
 
-          if (flake.y >= canvas.height || flake.y <= 0) {
-            reset(flake);
-          }
-
-          if (flake.x >= canvas.width || flake.x <= 0) {
-            reset(flake);
-          }
-
-          ctx.beginPath();
-          ctx.arc(flake.x, flake.y, flake.size, 0, Math.PI * 2);
-          ctx.fill();
-        }
-        requestAnimationFrame(snow);
-      }
-
-      function reset(flake) {
-        flake.x = Math.floor(Math.random() * canvas.width);
-        flake.y = 0;
-        flake.size = Math.random() * 3 + 2;
-        flake.speed = Math.random() * 1 + 0.4;
-        flake.velY = flake.speed;
-        flake.velX = 0;
-        flake.opacity = Math.random() * 0.5 + 0.1;
-      }
-
-      function init() {
-        for (var i = 0; i < flakeCount; i++) {
-          var x = Math.floor(Math.random() * canvas.width),
-            y = Math.floor(Math.random() * canvas.height),
-            size = Math.random() * 3 + 2,
-            speed = Math.random() * 1 + 0.4,
-            opacity = Math.random() * 0.5 + 0.1;
-
-          flakes.push({
-            speed: speed,
-            velY: speed,
-            velX: 0,
-            x: x,
-            y: y,
-            size: size,
-            stepSize: (Math.random() / 30) * 2,
-            step: 0,
-            angle: 180,
-            opacity: opacity,
+          window.requestAnimationFrame(function() {
+            _this.animate();
           });
+        },
+        init: function() {
+          this.resize(true);
+          this.animate();
         }
+      };
 
-        snow();
+      function Sakura(parent, x, y, scale, direction, rotate, wind) {
+        this.parent = parent;
+        this.x_pos = x;
+        this.y_pos = y;
+        this.scale = scale;
+        this.direction = direction;
+        this.rotate = rotate;
+        this.wind = wind;
+        this.gr = 5;
+        this.phase = 0;
       }
+      Sakura.prototype = {
+        draw: function(ctx) {
+          ctx.save();
+          ctx.beginPath();
+          ctx.translate(this.x_pos, this.y_pos);
 
-      document.addEventListener("mousemove", function(e) {
-        (mX = e.clientX), (mY = e.clientY);
-      });
-      window.addEventListener("resize", function() {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-      });
-      init();
-    },
+          ctx.rotate((this.direction.y / 100) * Math.PI);
+          ctx.scale(this.scale, this.scale);
+
+          var grad = ctx.createRadialGradient(0, 0, 0, 0, 0, 10);
+          grad.addColorStop(0, "#fbe0ef");
+          grad.addColorStop(0.1, "#f8c1d5");
+          grad.addColorStop(1, "#f3d3e2");
+          ctx.fillStyle = grad;
+
+          var x_rad = Math.cos((this.direction.x * Math.PI) / 100);
+          var z_rad = Math.cos((this.direction.z * Math.PI) / 100);
+          ctx.moveTo(-6 * z_rad, -10 * x_rad);
+          ctx.bezierCurveTo(
+            -10 * z_rad,
+            0 * x_rad,
+            -5 * z_rad,
+            10 * x_rad,
+            0 * z_rad,
+            10 * x_rad
+          );
+          ctx.bezierCurveTo(
+            5 * z_rad,
+            10 * x_rad,
+            10 * z_rad,
+            0 * x_rad,
+            6 * z_rad,
+            -10 * x_rad
+          );
+          ctx.bezierCurveTo(
+            0 * z_rad,
+            -10 * x_rad,
+            0 * z_rad,
+            -7 * x_rad,
+            0 * z_rad,
+            -5 * x_rad
+          );
+          ctx.bezierCurveTo(
+            0 * z_rad,
+            -7 * x_rad,
+            0 * z_rad,
+            -10 * x_rad,
+            -6 * z_rad,
+            -10 * x_rad
+          );
+          ctx.fill();
+          ctx.restore();
+
+          return this.moveSakura();
+        },
+        moveSakura: function() {
+          var move_y;
+          if (this.phase === 0) {
+            var ground = 1 + this.scale / 10;
+            if (this.y_pos > this.parent.height * ground) {
+              this.gr = 0;
+              this.wind = 0;
+              this.rotate.x = 0;
+              this.rotate.y = 0;
+              this.rotate.z = 0;
+              this.phase = 1;
+              this.parent.fallenSakura++;
+            }
+          } else if (this.phase === 2) {
+            if (this.gr > -3) this.gr += this.gr / 10;
+            move_y = this.gr * this.scale;
+          }
+
+          this.y_pos = this.y_pos + (this.gr * this.scale) / 2;
+          this.x_pos = this.x_pos + this.wind / 2;
+          this.direction.x += this.rotate.x / 2;
+          this.direction.y += this.rotate.y / 2;
+          this.direction.z += this.rotate.z / 2;
+
+          if (this.x_pos > this.parent.width) return true;
+          return this.y_pos > this.parent.height ? true : false;
+        }
+      };
+
+      var canvas = document.getElementById("Snow");
+      var SakuraCanvas = new Canvas(canvas);
+    }
   },
   mounted() {
     this.canvas();
-  },
+  }
 };
 </script>
 
@@ -124,12 +209,6 @@ export default {
   top: 0;
   left: 0;
   width: 100%;
-  height: 100%;
-}
-
-@media screen and (max-width: 768px) {
-  #Snow {
-    display: none;
-  }
+  height: auto;
 }
 </style>
