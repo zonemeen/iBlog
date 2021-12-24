1713bcbc5aedd29cbbe15b89711751d885d196cc
miqilin21 <994718917@qq.com>
2021-12-24 14:26:00 +0800

    chore: 修改安装、启动、打包项目方式

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -1,5 +1,8 @@
 // vue.config.js
 module.exports = {
+  devServer: {
+    port: 8090,
+  },
   outputDir: __dirname + '/../server/admin',
   publicPath: process.env.NODE_ENV === 'production' ?
     '/admin/' : '/'
