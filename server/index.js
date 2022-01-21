8017693ec166ca7c1ca2c53f98f0f93fcf746fae
miqilin21 <miqilin18@gmail.com>
2022-01-21 15:54:12 +0800

    chore: yarn代替npm

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const app = express()
 
-app.set("secret", "i2u34y12oi3u4y8");
+app.set('secret', 'i2u34y12oi3u4y8')
 
-app.use(cors());
-app.use(express.json());
-app.use("/uploads", express.static(__dirname + "/uploads"));
-app.use("/", express.static(__dirname + "/web"));
-app.use("/admin", express.static(__dirname + "/admin"));
+app.use(cors())
+app.use(express.json())
+app.use('/uploads', express.static(__dirname + '/uploads'))
+app.use('/', express.static(__dirname + '/web'))
+app.use('/admin', express.static(__dirname + '/admin'))
 
-require("./plugins/db")(app);
-require("./routes/admin/index.js")(app);
-require("./routes/web/index.js")(app);
+require('./plugins/db')(app)
+require('./routes/admin/index.js')(app)
+require('./routes/web/index.js')(app)
 
 app.listen(3000, async (req, res) => {
-  console.log("http://localhost:3000");
-});
+  console.log('http://localhost:3000')
+})
