c09672ac6fd5d6df0eae892bce20d31f7ebf400d
miqilin21 <miqilin18@gmail.com>
2022-01-21 14:02:22 +0800

    refactor: 删除QQ邮箱授权码配置

--- server/plugins/sendEmail.js
+++ server/plugins/sendEmail.js
@@ -1,6 +1,6 @@
 /**
  *
- * @Description 邮件发送 
+ * @Description 邮件发送
  * 调用方法:sendMail('994718917@qq.com','这是测试邮件', 'Hi miqilin,这是一封测试邮件');
  * @Author miqilin
  * @Created 2020/04/05 15:10
@@ -8,23 +8,25 @@
  *
  */
 
-const nodemailer = require('nodemailer');
-const smtpTransport = require('nodemailer-smtp-transport');
+const nodemailer = require('nodemailer')
+const smtpTransport = require('nodemailer-smtp-transport')
 const config = {
   email: {
     service: 'QQ', // 邮箱类别，我固定QQ
-    user: '994718917@qq.com',
-    pass: 'otyujutrdgsabfgd',
-  }
+    user: 'xxxxxxxxxx@qq.com', // 你的QQ邮箱，用来发送回复邮件
+    pass: 'xxxxxxxxxxxxx', // 你的QQ邮箱授权码，如何操作获取见README.md所述
+  },
 }
 
-sendSmtpTransport = nodemailer.createTransport(smtpTransport({
-  service: config.email.service,
-  auth: {
-    user: config.email.user,
-    pass: config.email.pass
-  }
-}));
+sendSmtpTransport = nodemailer.createTransport(
+  smtpTransport({
+    service: config.email.service,
+    auth: {
+      user: config.email.user,
+      pass: config.email.pass,
+    },
+  })
+)
 
 /**
  * @param {String} recipient 收件人
@@ -32,19 +34,20 @@ sendSmtpTransport = nodemailer.createTransport(smtpTransport({
  * @param {String} html 发送的html内容
  */
 var sendMail = function (replyObj) {
-
-  sendSmtpTransport.sendMail({
-    from: config.email.user,
-    to: replyObj.recipient,
-    subject: `${replyObj.subject} 你好! 你有新邮件了, 请查收`,
-    html: replyObj.html
-
-  }, function (error, response) {
-    if (error) {
-      console.log(error);
+  sendSmtpTransport.sendMail(
+    {
+      from: config.email.user,
+      to: replyObj.recipient,
+      subject: `${replyObj.subject} 你好! 你有新邮件了, 请查收`,
+      html: replyObj.html,
+    },
+    function (error, response) {
+      if (error) {
+        console.log(error)
+      }
+      console.log('发送成功')
     }
-    console.log('发送成功')
-  });
+  )
 }
 
-module.exports = sendMail;
+module.exports = sendMail
